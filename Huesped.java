5191a2d0293eba497c6d360424ec5f54fa25fb47
JoshueH9 <73609054+JoshueH9@users.noreply.github.com>
2024-02-25 19:19:32 -0600

    Add files via upload

---
+++ Huesped.java
@@ -0,0 +1,55 @@
+/**
+* Representacion de un huesped de hotel
+* @author Macal Cruz Brandon Brayan - 318085470
+* @author Pintor Munoz Pedro Joshue - 420053796
+* @author Rojo Trujillo Alessandro Gael - 317218385
+* @author Hernandez Morales Jose Angel - 315137903
+* @version 1.0 febrero 2024
+* @since Clase de Bases de datos 2024-2
+*/
+public class Huesped {
+	
+	/* Numero identificador del cliente (Identificador) */
+	private int id_huesped;
+	
+	/* Nombre del huesped */
+	private String nombre;
+
+	/* Apellido paterno del huesped */
+	private String apellido_paterno;
+	
+	/* Apellido materno del huesped */
+	private String apellido_materno;
+
+	/* Nacionalidad del huesped */
+	private String nacionalidad;
+
+	/* Fecha de nacimiento del huesped */
+	private String fecha_Nacimiento;
+
+	/* Genero del huesped */
+	private String genero;
+
+	/* Numero de membresia del huesped */
+	private int numero_membresia;
+
+	/* Correos electronicos del huesped */
+	private List<String> correos;
+
+	/* Lista de telefonos del huesped */
+	private List<Integer> telefonos;
+
+
+
+	// Getters
+
+	// Setters
+
+	// Metodo para agregar informacion
+
+	// Metodo para consultar
+
+	// Metodo para editar
+
+	// Metodo para eliminar informacion
+}
---
+++ main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ejemplo de uso
+        List<Hotel> hoteles = new ArrayList<>();
+
+        // Cargar datos desde CSV (ejemplo)
+        // CsvFileManager.loadHoteles(hoteles);
+        // CsvFileManager.loadHuespedes(huespedes);
+        // CsvFileManager.loadCuartos(cuartos);
+
+        // Realizar operaciones (ejemplo)
+        Hotel nuevoHotel = new Hotel(0, 0, 0, 0, 0, 0, null, null, null, null/* Datos del nuevo hotel */);
+        hoteles.add(nuevoHotel);
+
+        // Guardar datos en CSV
+        // CsvFileManager.saveHoteles(hoteles);
+        // CsvFileManager.saveHuespedes(huespedes);
+        // CsvFileManager.saveCuartos(cuartos);
+    }
+}
