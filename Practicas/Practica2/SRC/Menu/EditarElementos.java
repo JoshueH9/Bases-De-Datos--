e79e3dddfc220c6ea2bbeb105019dd059ae0a506
Brandon Macal <brandonmacal@ciencias.unam.mx>
2024-02-27 18:19:43 -0600

    V14

--- Practicas/Practica2/SRC/Menu/EditarElementos.java
+++ Practicas/Practica2/SRC/Menu/EditarElementos.java
@@ -69,15 +69,15 @@ public class EditarElementos implements OpcionMenu {
 
 				switch (menuUno) {
 					case 1: // Hotel
-						editarHotel(sc, hoteles);
+						pedirIdHotel(sc, hoteles);
 						break;
 
 					case 2: // Habitacion
-						editarHabitacion(sc, habitaciones);
+						pedirIdHabitacion(sc, habitaciones);
 						break;
 
 					case 3: // Huesped
-						editarHuesped(sc, huespedes);
+						pedirIdHuesped(sc, huespedes);
 						break;
 
 					default:
@@ -92,7 +92,99 @@ public class EditarElementos implements OpcionMenu {
 		} while (menuUno != 4);
 	}
 
-	private void editarHotel(Scanner sc, List<Hotel> hoteles) throws InterruptedException {
+	private void pedirIdHotel(Scanner sc, List<Hotel> hoteles) throws InterruptedException {
+		Boolean error = false;
+		do {
+			try {
+				System.out.print(Color.AMARILLO
+						+ "\n---------------- Ingresa los datos del hotel ----------------\n");
+				System.out.print(Color.AMARILLO + "\nIngresa el id del hotel a editar: "
+						+ Color.VERDE);
+				int idHotel = sc.nextInt();
+				Optional<Hotel> hotelEncontrado = hoteles.stream()
+						.filter(hotel -> hotel.getIdHotel() == idHotel)
+						.findFirst();
+
+				if (hotelEncontrado.isPresent()) {
+					Hotel hotelConsultado = hotelEncontrado.get();
+					editarHotel(sc, hotelConsultado);
+				} else {
+					throw new IllegalStateException("");
+				}
+
+			} catch (IllegalStateException e) {
+				System.out.println(
+						Color.ROJO + "\nERROR: No se pudo encontrar un hotel con el id proporcionado\n" + Color.BLANCO);
+				Thread.sleep(1500);
+				sc.nextLine();
+			}
+		} while (error);
+	}
+
+	private void pedirIdHabitacion(Scanner sc, List<Habitacion> habitaciones) throws InterruptedException {
+		Boolean error = false;
+		do {
+			try {
+				System.out.print(Color.AMARILLO
+						+ "\n---------------- Ingresa los datos de la habitacion ----------------\n");
+				System.out.print(Color.AMARILLO + "\nIngresa el numero de la habitacion: "
+						+ Color.VERDE);
+				int idHabitacion = sc.nextInt();
+
+				Optional<Habitacion> habitacionEncontrada = habitaciones.stream()
+						.filter(habitacion -> habitacion.getNumHabitacion() == idHabitacion)
+						.findFirst();
+
+				if (habitacionEncontrada.isPresent()) {
+					Habitacion habitacionConsultada = habitacionEncontrada.get();
+					editarHabitacion(sc, habitacionConsultada);
+					Thread.sleep(1500);
+				} else {
+					throw new InputMismatchException();
+				}
+			} catch (InputMismatchException ime) {
+				System.out.println(
+						Color.ROJO + "\nERROR: No se encontró ninguna habitacion con el numero proporcionado.\n"
+								+ Color.BLANCO);
+				Thread.sleep(1500);
+				error = true;
+				sc.nextLine();
+			}
+		} while (error);
+	}
+
+	private void pedirIdHuesped(Scanner sc, List<Huesped> huespedes) throws InterruptedException {
+		Boolean error = false;
+		do {
+			try {
+				System.out.print(Color.AMARILLO
+						+ "\n---------------- Ingresa los datos del cliente ----------------\n");
+				System.out.print(Color.AMARILLO + "\nIngresa el identificador del cliente: "
+						+ Color.VERDE);
+				int idHuesped = sc.nextInt();
+
+				Optional<Huesped> huespedEncontrado = huespedes.stream()
+						.filter(huesped -> huesped.getIdHuesped() == idHuesped)
+						.findFirst();
+
+				if (huespedEncontrado.isPresent()) {
+					Huesped huespedConsultado = huespedEncontrado.get();
+
+					editarHuesped(sc, huespedConsultado);
+				} else {
+					throw new IllegalStateException("");
+				}
+
+			} catch (IllegalStateException e) {
+				System.out.println(
+						Color.ROJO + "\nERROR: No se pudo encontrar un hotel con el id proporcionado\n" + Color.BLANCO);
+				Thread.sleep(1500);
+				sc.nextLine();
+			}
+		} while (error);
+	}
+
+	private void editarHotel(Scanner sc, Hotel hotel) throws InterruptedException {
 		Boolean error = false;
 		do {
 			try {
@@ -114,50 +206,117 @@ public class EditarElementos implements OpcionMenu {
 
 				switch (menuDos) {
 					case 1: // Nombre
-						editarHotel(sc, hoteles);
+						System.out.println(Color.AZUL + "\nNombre actual del hotel: " + Color.BLANCO
+								+ hotel.getNombreEstablecimiento());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo nombre del hotel: "
+								+ Color.VERDE);
+						String nombre = sc.next();
+						hotel.setNombreEstablecimiento(nombre);
 						break;
 
 					case 2: // Calle
-						editarHabitacion(sc, habitaciones);
+						System.out.println(Color.AZUL + "\nCalle actual del hotel: " + Color.BLANCO
+								+ hotel.getCalle());
+						System.out.print(Color.AMARILLO + "\nIngresa la nueva calle del hotel: "
+								+ Color.VERDE);
+						String calle = sc.next();
+						hotel.setCalle(calle);
+
 						break;
 
 					case 3: // Numero interior
-						editarHuesped(sc, huespedes);
+						System.out.println(Color.AZUL + "\nNumero interior actual del hotel: " + Color.BLANCO
+								+ hotel.getNumeroInterior());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo numero interior del hotel: "
+								+ Color.VERDE);
+						String numeroInterior = sc.next();
+						hotel.setNumeroInterior(numeroInterior);
 						break;
 
 					case 4: // Numero exterior
+						System.out.println(Color.AZUL + "\nNumero exterior actual del hotel: " + Color.BLANCO
+								+ hotel.getNombreEstablecimiento());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo numero exterior del hotel: "
+								+ Color.VERDE);
+						String numeroExterior = sc.next();
+						hotel.setNumeroExterior(numeroExterior);
+
 						break;
 
 					case 5: // Colonia
+						System.out.println(Color.AZUL + "\nColonia actual del hotel: " + Color.BLANCO
+								+ hotel.getColonia());
+						System.out.print(Color.AMARILLO + "\nIngresa la nueva colonia del hotel: "
+								+ Color.VERDE);
+						String colonia = sc.next();
+						hotel.setColonia(colonia);
 						break;
 
 					case 6: // Estado
+						System.out.println(Color.AZUL + "\nEstado actual del hotel: " + Color.BLANCO
+								+ hotel.getEstado());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo estado del hotel: "
+								+ Color.VERDE);
+						String estado = sc.next();
+						hotel.setEstado(estado);
 						break;
 
 					case 7: // Telefono
+						System.out.println(Color.AZUL + "\nTelefono actual del hotel: " + Color.BLANCO
+								+ hotel.getTelefono());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo telefono del hotel: "
+								+ Color.VERDE);
+						int telefono = sc.nextInt();
+						hotel.setTelefonos(telefono);
 						break;
 
 					case 8: // Numero de habitaciones
+						System.out.println(Color.AZUL + "\nNumero de habitaciones actuales del hotel: " + Color.BLANCO
+								+ hotel.getNumHabitaciones());
+						System.out.print(Color.AMARILLO + "\nIngresa el nuevo numero de habitaciones del hotel: "
+								+ Color.VERDE);
+						int numeroHabitaciones = sc.nextInt();
+						hotel.setNumHabitaciones(numeroHabitaciones);
 						break;
 
 					case 9: // Habitaciones disponibles
+						System.out.println(
+								Color.AZUL + "\nNumero de habitaciones disponibles actuales del hotel: " + Color.BLANCO
+										+ hotel.getNumHabitacionesDisponibles());
+						System.out.print(
+								Color.AMARILLO + "\nIngresa el nuevo numero de habitaciones disponibles del hotel: "
+										+ Color.VERDE);
+						int numeroHabitacionesDisponibles = sc.nextInt();
+						hotel.setNumHabitacionesDisponibles(numeroHabitacionesDisponibles);
 						break;
 
 					case 10: // Habitaciones ocupadas
+						System.out.println(
+								Color.AZUL + "\nNumero de habitaciones ocupadas actual del hotel: " + Color.BLANCO
+										+ hotel.getNumHabitacionesOcupadas());
+						System.out
+								.print(Color.AMARILLO + "\nIngresa el nuevo numero de habitaciones ocupadas del hotel: "
+										+ Color.VERDE);
+						int numeroHabitacionesOcupadas = sc.nextInt();
+						hotel.setNumHabitacionesOcupadas(numeroHabitacionesOcupadas);
 						break;
 					default:
-						break;
+						throw new InputMismatchException();
 				}
+				System.out.println(
+						Color.AZUL + "\nHotel editado con exito\n" + Color.BLANCO);
+				Thread.sleep(1500);
 
 			} catch (InputMismatchException ime) {
-				System.out.println(Color.ROJO + "\nERROR: Ingresa un numero.\n" + Color.BLANCO);
+				System.out.println(Color.ROJO + "\nERROR: Ingresa un numero. del 1 al 11\n" + Color.BLANCO);
+				error = true;
 				Thread.sleep(1500);
 				sc.nextLine();
 			}
 		} while (error);
 	}
 
-	private void editarHabitacion(Scanner sc, List<Habitacion> habitaciones) throws InterruptedException {
+	private void editarHabitacion(Scanner sc, Habitacion habitacion) throws InterruptedException {
 		Boolean error = false;
 		do {
 			try {
@@ -173,7 +332,7 @@ public class EditarElementos implements OpcionMenu {
 		} while (error);
 	}
 
-	private void editarHuesped(Scanner sc, List<Huesped> huespedes) throws InterruptedException {
+	private void editarHuesped(Scanner sc, Huesped huesped) throws InterruptedException {
 		Boolean error = false;
 		do {
 			try {
